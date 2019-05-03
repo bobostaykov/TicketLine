b7d22822d50d81bdd7405d29146f4ce9b34ef797
pino <e1619127@student.tuwien.ac.at>
2019-05-03 11:03:32 +0200

    Added exceptions correpsonding to Design Document PART 2 refs: #35084

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/exception/NotFoundException.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/exception/NotFoundException.java
@@ -6,4 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/exception/ServiceException.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.sepm.groupphase.backend.exception;
+
+public class ServiceException extends Exception{
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ServiceException(String message) {
+        super(message);
+    }
+}
+
