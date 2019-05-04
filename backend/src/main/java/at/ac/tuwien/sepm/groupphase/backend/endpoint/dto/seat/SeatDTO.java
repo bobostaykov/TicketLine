958490bece02f66a3f431dd502b515ae265df90e
Lukas Loidolt <lukas.loidolt@chello.at>
2019-05-04 21:43:19 +0200

    [#35090] implemented entity and dto classes for seat and sector

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/seat/SeatDTO.java
@@ -0,0 +1,125 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.seat;
+
+import at.ac.tuwien.sepm.groupphase.backend.datatype.PriceCategory;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "SeatDTO", description = "DTO for seat entities")
+public class SeatDTO {
+
+    @ApiModelProperty(name = "The automatically generated database id of the seat")
+    private Integer id;
+
+    @ApiModelProperty(name = "The seat number")
+    private Integer seatNumber;
+
+    @ApiModelProperty(name = "The seat row")
+    private Integer seatRow;
+
+    @ApiModelProperty(name = "The seat's price category. Either cheap, average or expensive.")
+    private PriceCategory priceCategory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(Integer seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Integer getSeatRow() {
+        return seatRow;
+    }
+
+    public void setSeatRow(Integer seatRow) {
+        this.seatRow = seatRow;
+    }
+
+    public PriceCategory getPriceCategory() {
+        return priceCategory;
+    }
+
+    public void setPriceCategory(PriceCategory priceCategory) {
+        this.priceCategory = priceCategory;
+    }
+
+    public static SeatDTOBuilder builder(){
+        return new SeatDTOBuilder();
+    }
+
+    @Override
+    public String toString() {
+        return "SeatDTO{" +
+            "id=" + id +
+            ", seatNumber=" + seatNumber +
+            ", seatRow=" + seatRow +
+            ", priceCategory='" + priceCategory + "\'" +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return  true;
+        if(o == null || getClass() != o.getClass()) return false;
+        SeatDTO seatDTO = (SeatDTO) o;
+        if(id != null ? ! id.equals(seatDTO.id) : seatDTO.id != null) return false;
+        if(seatNumber != null ? ! seatNumber.equals(seatDTO.getSeatNumber()) : seatDTO.getSeatNumber() != null) return false;
+        if(seatRow != null ? ! seatRow.equals(seatDTO.getSeatRow()) : seatDTO.getSeatRow() != null) return false;
+        return priceCategory != null ? priceCategory.equals(seatDTO.getPriceCategory()) : seatDTO.getPriceCategory() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (seatNumber != null ? seatNumber.hashCode() : 0);
+        result = 31 * result + (seatRow != null ? seatRow.hashCode() : 0);
+        result = 31 * result + (priceCategory != null ? priceCategory.hashCode() : 0);
+        return result;
+    }
+
+    public static class SeatDTOBuilder {
+        private Integer id;
+        private Integer seatNumber;
+        private Integer seatRow;
+        private PriceCategory priceCategory;
+
+        private SeatDTOBuilder(){}
+
+        public SeatDTOBuilder id(Integer id){
+            this.id = id;
+            return this;
+        }
+
+        public SeatDTOBuilder seatNumber(Integer seatNumber){
+            this.seatNumber = seatNumber;
+            return this;
+        }
+
+        public SeatDTOBuilder seatRow(Integer seatRow){
+            this.seatRow = seatRow;
+            return this;
+        }
+
+        public SeatDTOBuilder priceCategory(PriceCategory priceCategory){
+            this.priceCategory = priceCategory;
+            return this;
+        }
+
+        public SeatDTO build(){
+            SeatDTO seatDTO = new SeatDTO();
+            seatDTO.setId(id);
+            seatDTO.setSeatNumber(seatNumber);
+            seatDTO.setSeatRow(seatRow);
+            seatDTO.setPriceCategory(priceCategory);
+            return seatDTO;
+        }
+    }
+}
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Sector.java
@@ -0,0 +1,106 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import at.ac.tuwien.sepm.groupphase.backend.datatype.PriceCategory;
+
+import javax.persistence.*;
+
+@Entity
+public class Sector {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_sector_id")
+    @SequenceGenerator(name = "seq_sector_id", sequenceName = "seq_sector_id")
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer sectorNumber;
+
+    @Column(nullable = false)
+    private PriceCategory priceCategory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSectorNumber() {
+        return sectorNumber;
+    }
+
+    public void setSectorNumber(Integer sectorNumber) {
+        this.sectorNumber = sectorNumber;
+    }
+
+    public PriceCategory getPriceCategory() {
+        return priceCategory;
+    }
+
+    public void setPriceCategory(PriceCategory priceCategory) {
+        this.priceCategory = priceCategory;
+    }
+
+    public static SectorBuilder builder(){
+        return new SectorBuilder();
+    }
+
+    @Override
+    public String toString() {
+        return "Sector{" +
+            "id=" + id +
+            ", sectorNumber=" + sectorNumber +
+            ", priceCategory='" + priceCategory + "\'" +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return  true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Sector sector = (Sector) o;
+        if(id != null ? ! id.equals(sector.id) : sector.id != null) return false;
+        if(sectorNumber != null ? ! sectorNumber.equals(sector.getSectorNumber()) : sector.getSectorNumber() != null) return false;
+        return priceCategory != null ? priceCategory.equals(sector.getPriceCategory()) : sector.getPriceCategory() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (sectorNumber != null ? sectorNumber.hashCode() : 0);
+        result = 31 * result + (priceCategory != null ? priceCategory.hashCode() : 0);
+        return result;
+    }
+
+    public static final class SectorBuilder{
+        private Integer id;
+        private Integer sectorNumber;
+        private PriceCategory priceCategory;
+
+        private SectorBuilder(){}
+
+        public SectorBuilder id(Integer id){
+            this.id = id;
+            return this;
+        }
+
+        public SectorBuilder sectorNumber(Integer sectorNumber){
+            this.sectorNumber = sectorNumber;
+            return this;
+        }
+
+        public SectorBuilder priceCategory(PriceCategory priceCategory){
+            this.priceCategory = priceCategory;
+            return this;
+        }
+
+        public Sector build(){
+            Sector sector = new Sector();
+            sector.setId(id);
+            sector.setSectorNumber(sectorNumber);
+            sector.setPriceCategory(priceCategory);
+            return sector;
+        }
+    }
+}
