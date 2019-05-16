b1a07b8c506f03c3590b0016cee628c7f0fa0b62
boris <bobostaykov@gmail.com>
2019-05-16 22:10:51 +0200

    Exchange custom query for a predefined method in ArtistRepository

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ArtistRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ArtistRepository.java
@@ -2,8 +2,6 @@ package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,7 +14,5 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
      *
      * @return list of artists
      */
-    @Query("SELECT a FROM Artist a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :artistName, '%'))")
-    List<Artist> findArtistsByName(@Param("artistName") String artistName);
-
+    List<Artist> findByNameContainingIgnoreCase(String artistName);
 }
