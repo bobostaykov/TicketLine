9687e4aef8bc48d92bb48e761772733006057ab7
Lukas Loidolt <lukas.loidolt@chello.at>
2019-05-19 22:53:15 +0200

    [#35090] created form to add sectors, switched to reactive forms

--- frontend/src/app/components/floorplan-control/floorplan-control.component.ts
+++ frontend/src/app/components/floorplan-control/floorplan-control.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Seat} from '../../dtos/seat';
-import {PriceCategory} from '../../dtos/priceCategory';
 import {Sector} from '../../dtos/sector';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {PriceCategory} from '../../dtos/priceCategory';
 
 @Component({
   selector: 'app-floorplan-control',
@@ -9,50 +10,195 @@ import {Sector} from '../../dtos/sector';
   styleUrls: ['./floorplan-control.component.scss']
 })
 export class FloorplanControlComponent implements OnInit {
-  seats: Seat[] = [
-    new Seat(1, 1, 1, PriceCategory.Cheap),
-    new Seat(1, 2, 1, PriceCategory.Cheap),
-    new Seat(1, 3, 1, PriceCategory.Cheap),
-    new Seat(1, 4, 1, PriceCategory.Cheap),
-    new Seat(1, 5, 1, PriceCategory.Cheap),
-    new Seat(1, 6, 1, PriceCategory.Cheap),
-    new Seat(1, 7, 1, PriceCategory.Cheap),
-    new Seat(1, 8, 1, PriceCategory.Cheap),
-    new Seat(1, 9, 1, PriceCategory.Cheap),
-    new Seat(1, 10, 1, PriceCategory.Cheap),
-  ];
-  sectors: Sector[] = [
-    new Sector(1, 1, PriceCategory.Average),
-    new Sector(1, 2, PriceCategory.Cheap),
-    new Sector(1, 3, PriceCategory.Average),
-    new Sector(1, 4, PriceCategory.Average),
-    new Sector(1, 5, PriceCategory.Average),
-    new Sector(1, 6, PriceCategory.Average),
-    new Sector(1, 7, PriceCategory.Average),
-    new Sector(1, 8, PriceCategory.Average),
-    new Sector(1, 9, PriceCategory.Expensive),
-  ];
-  private seatFormModel = {
-    seatRowStart: null,
-    seatRowEnd: null,
-    seatNumberStart: null,
-    seatNumberEnd: null
-  };
 
-  ngOnInit(): void {
+  private seats: Seat[] = [];
+  private sectors: Sector[];
+  private priceCategories: string[] = Object.keys(PriceCategory);
+  private addSeatsForm: FormGroup;
+  private addSectorsForm: FormGroup;
+
+  constructor() {
   }
 
-  private addSeats() {
-    const seatRowStart = Math.min(this.seatFormModel.seatRowStart, this.seatFormModel.seatRowEnd);
-    const seatRowEnd = Math.max(this.seatFormModel.seatRowStart, this.seatFormModel.seatRowEnd);
-    const seatNumberStart = Math.min(this.seatFormModel.seatNumberStart, this.seatFormModel.seatNumberEnd);
-    const seatNumberEnd = Math.max(this.seatFormModel.seatNumberStart, this.seatFormModel.seatNumberEnd);
-    for (let row = seatRowStart; row <= seatRowEnd; row++) {
-      for (let number = seatNumberStart; number < seatNumberEnd; number++) {
-        if (! this.seats.some(seat => seat.seatRow === row && seat.seatNumber === number)) {
-          this.seats.push(new Seat(null, number, row, PriceCategory.Average));
+  /**
+   * run on initialization of component
+   * initializes form groups for addSeat and addSector
+   */
+  ngOnInit(): void {
+    this.buildSeatForm();
+    this.buildSectorForm();
+  }
+
+  /**
+   * called on submit of addSeatsForm
+   * adds seats according to parameters entered by user
+   * afterwards resets form
+   */
+  private addSeats(): void {
+    if (this.addSeatsForm.valid) {
+      const value = this.addSeatsForm.value;
+      // if one of the seat row or number values is null, set equal to other seat row/number value
+      value.seatRowStart = value.seatRowStart === null ? value.seatRowEnd : value.seatRowStart;
+      value.seatRowEnd = value.seatRowEnd === null ? value.seatRowStart : value.seatRowEnd;
+      value.seatNumberStart = value.seatNumberStart === null ? value.seatNumberEnd : value.seatNumberStart;
+      value.seatNumberEnd = value.seatNumberEnd === null ? value.seatNumberStart : value.seatNumberEnd;
+      // determine which value is min and which is max to run loop correctly afterwards
+      value.seatRowStart = Math.min(value.seatRowStart, value.seatRowEnd);
+      value.seatRowEnd = Math.max(value.seatRowStart, value.seatRowEnd);
+      value.seatNumberStart = Math.min(value.seatNumberStart, value.seatNumberEnd);
+      value.seatNumberEnd = Math.max(value.seatNumberStart, value.seatNumberEnd);
+      for (let row = value.seatRowStart; row <= value.seatRowEnd; row++) {
+        for (let number = value.seatNumberStart; number <= value.seatNumberEnd; number++) {
+          if (!this.seats.some(seat => seat.seatRow === row && seat.seatNumber === number)) {
+            this.seats.push(new Seat(null, number, row, value.seatPrice));
+          }
         }
       }
+      this.addSeatsForm.reset({
+        'seatPrice': PriceCategory.Cheap
+      });
     }
+  }
+
+  /**
+   * called on submit of addSectorsForm
+   * adds sectors according to parameters entered by user
+   * afterwards resets form
+   */
+  private addSectors(): void {
+    if (this.addSectorsForm.valid) {
+      const value = this.addSectorsForm.value;
+      // if one of the sector number values is null, set equal to other seat row/number value
+      value.sectorNumberStart = value.sectorNumberStart === null ? value.sectorNumberEnd : value.sectorNumberStart;
+      value.sectorNumberEnd = value.sectorNumberEnd === null ? value.sectorNumberStart : value.sectorNumberEnd;
+      // determine which value is min and which is max to run loop correctly afterwards
+      value.sectorNumberStart = Math.min(value.sectorNumberStart, value.sectorNumberEnd);
+      value.sectorNumberEnd = Math.max(value.sectorNumberStart, value.sectorNumberEnd);
+      for (let number = value.sectorNumberStart; number <= value.sectorNumberEnd; number++) {
+        if (! this.sectors.some(sector => sector.sectorNumber === number)) {
+          this.sectors.push(new Sector(null, number, value.sectorPrice));
+        }
+      }
+      this.addSectorsForm.reset({
+        'sectorPrice': PriceCategory.Cheap
+      });
+    }
+  }
+
+  /**
+   * called after initialization of component
+   * initializes addSeatsForm
+   * also notes value changes in form inputs and sets validation accordingly
+   * for seatRow and seatNumber only one of start/end has to be set
+   */
+  private buildSeatForm(): void {
+    this.addSeatsForm = new FormGroup({
+      'seatRowStart': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'seatRowEnd': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'seatNumberStart': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'seatNumberEnd': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'seatPrice': new FormControl(PriceCategory.Cheap, [Validators.required])
+    });
+    const seatRowStart = this.addSeatsForm.get('seatRowStart');
+    const seatRowEnd = this.addSeatsForm.get('seatRowEnd');
+    const seatNumberStart = this.addSeatsForm.get('seatNumberStart');
+    const seatNumberEnd = this.addSeatsForm.get('seatNumberEnd');
+
+    seatRowStart.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        seatRowEnd.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        seatRowEnd.setValidators(Validators.min(1));
+      }
+      seatRowEnd.updateValueAndValidity({emitEvent: false});
+    });
+    seatRowEnd.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        seatRowStart.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        seatRowStart.setValidators(Validators.min(1));
+      }
+      seatRowStart.updateValueAndValidity({emitEvent: false});
+    });
+    seatNumberStart.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        seatNumberEnd.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        seatNumberEnd.setValidators(Validators.min(1));
+      }
+      seatNumberEnd.updateValueAndValidity({emitEvent: false});
+    });
+    seatNumberEnd.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        seatNumberStart.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        seatNumberStart.setValidators(Validators.min(1));
+      }
+      seatNumberStart.updateValueAndValidity({emitEvent: false});
+    });
+  }
+
+  /**
+   * returns true, if inputs seatRowStart or seatRowError have been edited and are invalid
+   * called to determine if error message needs to be displayed
+   */
+  private seatRowErrors(): boolean {
+    const seatRowStart = this.addSeatsForm.get('seatRowStart');
+    const seatRowEnd = this.addSeatsForm.get('seatRowEnd');
+    console.log((seatRowStart.dirty || seatRowEnd.dirty));
+    return (seatRowStart.dirty || seatRowEnd.dirty) && (seatRowStart.invalid || seatRowEnd.invalid);
+  }
+
+  /**
+   * returns ture, if inputs seatNumberStart or seatNumberEnd have been edited and are invalid
+   * called to determine if error message needs to be displayed
+   */
+  private seatNumberErrors(): boolean {
+    const seatNumberStart = this.addSeatsForm.get('seatNumberStart');
+    const seatNumberEnd = this.addSeatsForm.get('seatNumberEnd');
+    return (seatNumberStart.dirty || seatNumberEnd.dirty) && (seatNumberStart.invalid || seatNumberEnd.invalid);
+  }
+
+  /**
+   * called after initialization of component
+   * initializes addSectorsForm and determines also notes value changes to set validators accordingly
+   * only one of sectorNumberStart and sectorNumberEnd have to be set
+   */
+  private buildSectorForm(): void {
+    this.addSectorsForm = new FormGroup({
+      'sectorNumberStart': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'sectorNumberEnd': new FormControl(null, [Validators.required, Validators.min(1)]),
+      'sectorPrice': new FormControl(PriceCategory.Cheap, [Validators.required])
+    });
+
+    const sectorNumberStart = this.addSectorsForm.get('sectorNumberStart');
+    const sectorNumberEnd = this.addSectorsForm.get('sectorNumberEnd');
+
+    sectorNumberStart.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        sectorNumberEnd.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        sectorNumberEnd.setValidators(Validators.min(1));
+      }
+      sectorNumberEnd.updateValueAndValidity({emitEvent: false});
+    });
+    sectorNumberEnd.valueChanges.subscribe(seat => {
+      if (seat === null) {
+        sectorNumberStart.setValidators([Validators.required, Validators.min(1)]);
+      } else {
+        sectorNumberStart.setValidators(Validators.min(1));
+      }
+      sectorNumberStart.updateValueAndValidity({emitEvent: false});
+    });
+  }
+
+  /**
+   * returns true if sectorNumberStart or sectorNumberEnd have been edited and are invalid
+   * called to determine if error message needs to be displayed
+   */
+  private sectorNumberErrors(): boolean {
+    const sectorNumberStart = this.addSectorsForm.get('sectorNumberStart');
+    const sectorNumberEnd = this.addSectorsForm.get('sectorNumberEnd');
+    return (sectorNumberStart.dirty || sectorNumberEnd.dirty) && (sectorNumberStart.invalid || sectorNumberEnd.invalid);
   }
 }
