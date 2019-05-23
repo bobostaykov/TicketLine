8a1b82dd15fb358dc03d2d7aa2cd4e8e9c02e0f2
Lukas Loidolt <lukas.loidolt@chello.at>
2019-05-24 00:18:29 +0200

    [#35090] added admin authorization for postHall and swagger annotations

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/HallEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/HallEndpoint.java
@@ -4,9 +4,12 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.hall.HallDTO;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
 import at.ac.tuwien.sepm.groupphase.backend.service.HallService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -25,13 +28,15 @@ public class HallEndpoint {
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all saved halls", authorizations = {@Authorization(value = "apiKey")})
     public List<HallDTO> getHalls(){
         LOGGER.info("GET Halls: all Halls");
         return hallService.findAllHalls();
     }
 
-    //TODO: add authorization
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    @ApiOperation(value = "Add a new Hall with seats or sectors", authorizations = {@Authorization(value = "apiKey")})
     public HallDTO postHall(@RequestBody HallDTO hallDto) {
         LOGGER.info("POST Halls: " + hallDto.toString());
         try {
