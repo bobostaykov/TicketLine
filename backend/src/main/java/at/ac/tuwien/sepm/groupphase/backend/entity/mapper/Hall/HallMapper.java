453a84ceee97ef4c702ded88c97e67a8e88835ff
Lukas-96 <lukas.loidolt@chello.at>
2019-05-23 21:41:54 +0200

    [#35090] added method to convert list of halls to list of hall dtos in HallMapper

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/Hall/HallMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/Hall/HallMapper.java
@@ -4,10 +4,14 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.hall.HallDTO;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Hall;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface HallMapper {
 
     Hall hallDTOToHall(HallDTO hallDTO);
 
     HallDTO hallToHallDTO(Hall hall);
+
+    List<HallDTO> hallListToHallDTOs(List<Hall> halls);
 }
