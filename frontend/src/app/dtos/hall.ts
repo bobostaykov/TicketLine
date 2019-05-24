05a3c66b5eb963538ab8505f4596a4c6c80364f5
Lukas Loidolt <lukas.loidolt@chello.at>
2019-05-24 11:52:56 +0200

    [#35094] added seats and sectors list to hall dto frontend and removed list of shows

--- frontend/src/app/dtos/hall.ts
+++ frontend/src/app/dtos/hall.ts
@@ -1,11 +1,15 @@
 import {Show} from './show';
 import {Location} from './location';
+import {Seat} from './seat';
+import {Sector} from './sector';
 
 export class Hall {
   constructor(
     public id: number,
     public name: string,
-    public shows: Show[],
-    public location: Location
+    // public shows: Show[], TODO: do we need a list of shows here
+    public location: Location,
+    public seats: Seat[],
+    public sector: Sector[]
   ) {}
 }
