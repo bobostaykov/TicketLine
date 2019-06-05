c3e64d12774d574331c98381b344faa28f582700
Katharina Bajic <e01529014@student.tuwien.ac.at>
2019-06-05 16:28:59 +0200

    [#35104] create componenent, dto etc. for tickets

---
+++ frontend/src/app/services/ticket/ticket.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Ticket} from '../../dtos/ticket';
+import {Observable} from 'rxjs';
+import {Globals} from '../../global/globals';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  private ticketBaseUri: string = this.globals.backendUri + '/ticket';
+
+  constructor(private httpClient: HttpClient, private globals: Globals) {
+  }
+
+  /**
+   * Loads all tickets from the backend
+   */
+  getAllTickets(): Observable<Ticket[]> {
+    console.log('Get all tickets');
+    return this.httpClient.get<Ticket[]>(this.ticketBaseUri);
+  }
+
+  /**
+   * Loads specific ticket from the backend
+   * @param id of ticket to load
+   */
+  getTicketById(id: number): Observable<Ticket> {
+    console.log('Load ticket for ' + id);
+    return this.httpClient.get<Ticket>(this.ticketBaseUri + '/' + id);
+  }
+
+  /**
+   * Persists ticket to the backend
+   * @param ticket to persist
+   */
+  createTicket(ticket: Ticket): Observable<Ticket> {
+    console.log('Create ticket');
+    return this.httpClient.post<Ticket>(this.ticketBaseUri, ticket);
+  }
+}
