f60defbd0b02a982bf9af7b6edab23c6a33fbc15
Katharina Bajic <e01529014@student.tuwien.ac.at>
2019-06-08 10:44:18 +0200

    [#35104] fixed bug where ticket component was imported twice in app module, deleted munnecessary code in ticket check reservation html

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -55,7 +55,7 @@ import { SelectDropDownModule } from 'ngx-select-dropdown'
     FloorplanComponent,
     CustomerComponent,
     CustomerDialogComponent,
-    TicketComponent,
+    CustomerDialogComponent,
     BlockedUsersComponent,
     TicketCheckReservationComponent,
     ContentManagerComponent
