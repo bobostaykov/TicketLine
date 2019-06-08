00feca0419e235791eff6c72f5183406f0cd85a0
Lukas Loidolt <lukas.loidolt@chello.at>
2019-06-08 16:21:41 +0200

    [#35086] finished refactoring floorplan, implemented update and delete via contextmenu frontend, started implementing ticket functionality

--- frontend/src/app/components/floorplan/floorplan-svg/floorplan-svg.component.ts
+++ frontend/src/app/components/floorplan/floorplan-svg/floorplan-svg.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Seat} from '../../../dtos/seat';
 import {Sector} from '../../../dtos/sector';
 import {PriceCategory} from '../../../dtos/priceCategory';
@@ -11,33 +11,26 @@ import {PriceCategory} from '../../../dtos/priceCategory';
 export class FloorplanSvgComponent implements OnInit {
   @Input() private seats?: Seat[];
   @Input() private sectors?: Sector[];
-  @Output() private selectedSeat: EventEmitter<{eventTarget: HTMLElement, seat: Seat}> = new EventEmitter();
-  @Output() private selectedSector: EventEmitter<{eventTarget: HTMLElement, sector: Sector}> = new EventEmitter();
   private viewboxPosX: number = 0;
   private viewboxPosY: number = 0;
   private viewboxWidth: number = 500;
   private viewbox: string = this.viewboxPosX + ' ' + this.viewboxPosY + ' ' + this.viewboxWidth + ' ' + this.viewboxWidth;
+  // list of priceCategories to loop through in select fields
+  // noinspection JSMismatchedCollectionQueryUpdate
+  private priceCategories: string[] = Object.keys(PriceCategory);
+  private selectedElement: Seat | Sector;
+  private updateElementModel: Seat | Sector = this.seats ? new Seat(null, null, null, null) : new Sector(null, null, null);
+
 
   constructor() {
   }
-
-  private selectSeat(event: Event, seat: Seat): void {
-    this.selectedSeat.emit({eventTarget: <HTMLElement> event.target, seat: seat});
-  }
-
-  private selectSector(event: Event, sector: Sector): void {
-    this.selectedSector.emit({eventTarget: <HTMLElement> event.target, sector: sector});
-  }
-
 
   /**
    * gets string for d attribute of svg path element depicting a seat
    * @param seat for which to return path attribute
    */
   private getSeatPath(seat: Seat): string {
-    const xPos: number = (seat.seatNumber - 1) * 1.2 * 10 + Math.floor(seat.seatNumber / 15) * 10;
-    const yPos: number = seat.seatRow * 1.2 * 10 + Math.floor(seat.seatRow / 10) * 10;
-    return 'M' + xPos + ' ' + yPos + 'h 10 v 10 h -10 Z';
+    return 'M' + this.getSeatXPos(seat) + ' ' + this.getSeatYPos(seat) + 'h 10 v 10 h -10 Z';
   }
 
   /**
@@ -46,10 +39,7 @@ export class FloorplanSvgComponent implements OnInit {
    */
   private getSectorPath(sector: Sector): string {
     const width = this.viewboxWidth / 3.2;
-    const gap = this.viewboxWidth / 32;
-    const xPos: number = ((sector.sectorNumber - 1) % 3) * (width + gap);
-    const yPos: number = Math.floor((sector.sectorNumber - 1) / 3) * (50 + gap);
-    return 'M' + xPos + ' ' + yPos + 'h ' + width + ' v 50 h ' + (-width) + ' Z';
+    return 'M' + this.getSectorXPos(sector) + ' ' + this.getSectorYPos(sector) + 'h ' + width + ' v 50 h ' + (-width) + ' Z';
   }
 
   /**
@@ -61,10 +51,36 @@ export class FloorplanSvgComponent implements OnInit {
     return elem.priceCategory === PriceCategory.Cheap ? '#2fb207' : (elem.priceCategory === PriceCategory.Average ? '#129ded' : '#db0611');
   }
 
+  private displayUpdateForm(element: Seat | Sector): void {
+    const svg = document.getElementsByTagName('svg')[0];
+    if (svg instanceof SVGSVGElement) {
+      this.closeContext();
+      const updateForm = document.getElementById('updateForm');
+      updateForm.style.display = 'inline-block';
+      const rectForm = updateForm.getBoundingClientRect();
+      const rectSvg = svg.getBoundingClientRect();
+      const svgpx = svg.getCTM().a;
+      const elementHeight = 10 * svgpx;
+      const elementWidth = 10 * svgpx;
+      const xPos = element instanceof Seat ? this.getSeatXPos(element) : this.getSectorXPos(element);
+      const yPos = element instanceof Seat ? this.getSeatYPos(element) : this.getSectorYPos(element);
+      updateForm.style.left = rectSvg.left + svgpx * xPos - rectForm.width / 2 + elementWidth / 2 + 'px';
+      updateForm.style.top = rectSvg.top + svgpx * yPos + elementHeight + 20 + 'px';
+      this.selectedElement = element;
+      Object.assign(this.updateElementModel, this.selectedElement);
+    }
+  }
+
+  private closeUpdateForm(): void {
+    document.getElementById('updateForm').style.display = 'none';
+  }
+
+
   /**
    * allows users to zoom in by adjusting svg viewbox
    * @param event mousewheel event used to zoom
    */
+
   /*private zoom(event) {
     event.preventDefault();
     this.viewboxWidth = event.deltaY > 0 ? this.viewboxWidth * 1.05 : this.viewboxWidth / 1.05;
@@ -77,6 +93,64 @@ export class FloorplanSvgComponent implements OnInit {
     this.viewbox = this.viewboxPosX + ' ' + this.viewboxPosY + ' ' + this.viewboxWidth + ' ' + this.viewboxWidth;
   }*/
 
+  private getSeatXPos(seat: Seat): number {
+    return (seat.seatNumber - 1) * 1.2 * 10 + Math.floor(seat.seatNumber / 15) * 10;
+  }
+
+  private getSeatYPos(seat: Seat): number {
+    return seat.seatRow * 1.2 * 10 + Math.floor(seat.seatRow / 10) * 10;
+  }
+
+  private getSectorXPos(sector: Sector): number {
+    const width = this.viewboxWidth / 3.2;
+    const gap = this.viewboxWidth / 32;
+    return ((sector.sectorNumber - 1) % 3) * (width + gap);
+  }
+
+  private getSectorYPos(sector: Sector): number {
+    const gap = this.viewboxWidth / 32;
+    return Math.floor((sector.sectorNumber - 1) / 3) * (50 + gap);
+  }
+
   ngOnInit() {
+  }
+
+  private displayContext(element: Seat | Sector, event: MouseEvent): void {
+    event.preventDefault();
+    if (event.target instanceof Element) {
+      this.closeUpdateForm();
+      const rectEvent = event.target.getBoundingClientRect();
+      const context = document.getElementById('contextmenu');
+      context.style.left = rectEvent.left + 'px';
+      context.style.top = rectEvent.bottom + 'px';
+      context.style.display = 'inline-block';
+      this.selectedElement = element;
+    }
+  }
+
+  private closeContext(): void {
+    document.getElementById('contextmenu').style.display = 'none';
+  }
+
+  private onSvgClick(event: Event): void {
+    if ((event.target as HTMLElement).tagName !== 'path') {
+      this.closeUpdateForm();
+      this.closeContext();
+    }
+  }
+
+  private deleteSelectedElement(): void {
+    if (this.selectedElement instanceof Seat) {
+      this.seats.splice(this.seats.indexOf(this.selectedElement), 1);
+      this.closeContext();
+    }
+  }
+
+  private addToTicket(): void {
+  }
+
+  private updateSelectedElement(): void {
+    Object.assign(this.selectedElement, this.updateElementModel);
+    this.displayUpdateForm(this.selectedElement);
   }
 }
