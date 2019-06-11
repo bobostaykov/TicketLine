49c1027575a3112693f7621c8fb709ea28ca19bb
Katharina Bajic <e01529014@student.tuwien.ac.at>
2019-06-11 18:29:32 +0200

    [#35114] added component in frontend to check reservatin infos

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/TicketEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/TicketEndpoint.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Authorization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class TicketEndpoint {
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @PreAuthorize("hasRole('User')")
     @ApiOperation(value = "Create a ticket", authorizations = {@Authorization(value = "apiKey")})
     public TicketDTO create(@RequestBody TicketDTO ticketDTO) {
         LOGGER.info("Create Ticket");
--- frontend/src/app/components/ticket/ticket-check-reservation/ticket-check-reservation.component.ts
+++ frontend/src/app/components/ticket/ticket-check-reservation/ticket-check-reservation.component.ts
@@ -27,10 +27,7 @@ export class TicketCheckReservationComponent implements OnInit {
   errorMessage: string = '';
   ticketCheckReservationForm: FormGroup;
   submitted: boolean = false;
-  private ticket: Ticket;
-  private show: Show;
-  private customer: Customer;
-  private seat: Seat;
+  // TODO: delete default objects
   private sector: Sector;
   private event: Event;
   private hall: Hall;
@@ -38,23 +35,84 @@ export class TicketCheckReservationComponent implements OnInit {
   private location: Location;
   private seats: Seat[];
   private sectors: Sector[];
+  // TODO: End of default objects
+  private price: number[] = [];
+  private priceTotal: number;
+  private show: Show;
+  private customer: Customer;
+  private ticket_seats: Seat[] = [];
+  private ticket_sectors: Sector[] = [];
+  private tickets: Ticket[] = [];
+  private amtTickets: number;
+  private seatsStr: String[] = [];
+  private rowStr: String[] = [];
+  private idxPrice: number;
 
   constructor(private ticketService: TicketService, private ngbPaginationConfig: NgbPaginationConfig, private formBuilder: FormBuilder,
               private cd: ChangeDetectorRef, private authService: AuthService) {}
 
   ngOnInit() {
     // TODO: delete default objects
-    this.location = new Location(9, 'Austria', 'Vienna', '1180', 'testStreet',
+    this.location = new Location(9, 'Austria', 'Vienna', '1150', 'Roland-Rainer-Platz 1',
       'description for lcoation default test');
-    this.artist = new Artist(8, 'ArtistTestName');
-    this.hall = new Hall(7, 'HallTestName', this.location, this.seats, this.sectors);
-    this.event = new Event(6, 'eventTestName', EventType.CONCERT, 'description for this default test event',
-      'content for thie default test event', this.artist);
+    this.artist = new Artist(8, 'Cher');
+    this.hall = new Hall(7, 'Stadthalle A', this.location, this.seats, this.sectors);
+    this.event = new Event(6, 'Here we go again', EventType.CONCERT, 'description for this default test event',
+      'content for this default test event', this.artist);
     this.sector = new Sector(5, 2, PriceCategory.Average);
-    this.seat = new Seat(4, 15, 33, PriceCategory.Average);
-    this.customer = new Customer(3, 'TestName', 'TestFirstName', 'Test@email.com', '05.05.2005');
+    this.customer = new Customer(3, 'Müller', 'Petra', 'petra.mueller@email.com', '05.05.2005');
     this.show = new Show(2, this.event, '20:20', '12.05.2019', this.hall, 'description for this default test show', 44);
-    this.ticket = new Ticket(1, this.show, this.customer, 22.22, this.seat, this.sector, 'reservation');
+    this.ticket_seats.push(new Seat(10, 14, 22, PriceCategory.Average));
+    this.ticket_seats.push(new Seat(10, 15, 22, PriceCategory.Average));
+    this.price.push(35.99);
+    this.price.push(35.99);
+    // TODO: End of default objects
+    this.idxPrice = 0;
+    this.priceTotal = 0;
+
+    if (this.ticket_seats.length > 0) {
+      this.amtTickets = this.ticket_seats.length;
+      for (const entry of this.ticket_seats) {
+        this.seatsStr.push(entry.seatNumber.toString());
+        this.rowStr.push(entry.seatRow.toString());
+        this.tickets.push(new Ticket(null, this.show, this.customer, this.price[this.idxPrice], entry, null, 'reservation'));
+        this.idxPrice += 1;
+        this.priceTotal += this.price.pop();
+      }
+    }
+
+    if (this.ticket_sectors.length > 0) {
+      this.amtTickets = this.ticket_sectors.length;
+      for (const entry of this.ticket_sectors) {
+        this.tickets.push(new Ticket(null, this.show, this.customer, this.price[this.idxPrice], null, entry, 'reservation'));
+        this.idxPrice += 1;
+        this.priceTotal += this.price.pop();
+      }
+    }
+  }
+
+  /**
+   * Starts form validation and builds a news dto for sending a creation request if the form is valid.
+   * If the procedure was successful, the form will be cleared.
+   */
+  createTicket() {
+    this.submitted = true;
+    for (const entry of this.tickets) {
+      this.addTicket(entry);
+    }
+  }
+
+  /**
+   * Sends news creation request
+   * @param news the news which should be created
+   */
+  addTicket(ticket: Ticket) {
+    this.ticketService.createTicket(ticket).subscribe(
+      () => {},
+      error => {
+        this.defaultServiceErrorHandling(error);
+      }
+    );
   }
 
   /**
