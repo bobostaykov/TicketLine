39ba9fbf7436ece1f0810791bae0f79332ea3c29
Lukas Loidolt <lukas.loidolt@chello.at>
2019-06-11 19:43:53 +0200

    [#37610] built component representing selected tickets

--- frontend/src/app/components/floorplan/floorplan-ticket/floorplan-ticket.component.ts
+++ frontend/src/app/components/floorplan/floorplan-ticket/floorplan-ticket.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Sector} from '../../../dtos/sector';
+import {Seat} from '../../../dtos/seat';
 
 @Component({
   selector: 'app-floorplan-ticket',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FloorplanTicketComponent implements OnInit {
 
-  constructor() { }
+  @Input() private tickets: Array<Seat | Sector>;
+  private selectedTicket: Seat | Sector;
+  private _closeContext = this.closeContext.bind(this);
+
+  constructor() {
+  }
+
 
   ngOnInit() {
   }
 
+  private deleteSelectedTicket(): void {
+    this.tickets.splice(this.tickets.indexOf(this.selectedTicket), 1);
+  }
+
+  private displayContextmenu(ticket: Seat | Sector, event: MouseEvent): void {
+    event.preventDefault();
+    if (event.target instanceof Element) {
+      const context = document.getElementById('contextmenuTicket');
+      context.style.left = event.pageX + 'px';
+      context.style.top = event.pageY + 'px';
+      context.style.display = 'inline-block';
+      this.selectedTicket = ticket;
+      document.addEventListener('click', this._closeContext);
+    }
+  }
+
+  private closeContext(event: MouseEvent): void {
+    if (! (event.target as HTMLElement).closest('.ticket')) {
+      document.getElementById('contextmenuTicket').style.display = 'none';
+      document.removeEventListener('click', this._closeContext);
+    }
+  }
 }
