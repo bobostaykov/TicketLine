47dc2c3248d74a6e9df82ffe7b41e0257df901c9
pino <e1619127@student.tuwien.ac.at>
2019-06-23 14:35:09 +0200

    Added real ticketsSold values to demo and performanceTest DataGenerators and increased number of tickets for demo (with random shows to have a good demo for top ten events page refs: #40441

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/performanceTest/TicketPerformanceTestDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/performanceTest/TicketPerformanceTestDataGenerator.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.datagenerator.performanceTest;
 
 import at.ac.tuwien.sepm.groupphase.backend.datatype.TicketStatus;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Show;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Ticket;
 import at.ac.tuwien.sepm.groupphase.backend.repository.CustomerRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ShowRepository;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @Profile("generatePerformanceTestData")
 @Component
@@ -24,8 +27,6 @@ public class TicketPerformanceTestDataGenerator extends PerformanceTestDataGener
     private final TicketRepository ticketRepository;
     private final CustomerRepository customerRepository;
     private final ShowRepository showRepository;
-
-    private final static int NUM_OF_TICKETS = 2;
 
     public TicketPerformanceTestDataGenerator(TicketRepository ticketRepository,
                                               CustomerRepository customerRepository,
@@ -41,13 +42,21 @@ public class TicketPerformanceTestDataGenerator extends PerformanceTestDataGener
             LOGGER.info("Tickets already generated");
         }else {
             LOGGER.info("Generating {} tickets", NUM_OF_TICKETS);
+            Random randomGenerator = new Random();
             List<Ticket> tickets = new ArrayList<>();
+            Optional<Show> showOptional;
             for(Long id = 1L; id <= NUM_OF_TICKETS; id++) {
+                do {
+                    showOptional = showRepository.findById(customMod(id + randomGenerator.nextInt(NUM_OF_SHOWS), NUM_OF_SHOWS));
+                } while (showOptional.isEmpty());
+                Show show = showOptional.get();
+                show.setTicketsSold(show.getTicketsSold() + 1);
+                show = showRepository.save(show);
                 tickets.add(Ticket.builder()
                     .id(id)
                     .customer(customerRepository.getOne(customMod(id, NUM_OF_CUSTOMERS)))
-                    .show(showRepository.getOne(customMod(id, NUM_OF_SHOWS)))
-                    .status(id % 2 == 0 ? TicketStatus.SOLD : TicketStatus.RESERVATED)
+                    .show(show)
+                    .status(id % 6 == 0 ? TicketStatus.SOLD : TicketStatus.RESERVATED)
                     //.seatNumber(customModInt(id, NUM_OF_SEAT_ROWS_PER_HALL))
                     //.rowNumber(Math.toIntExact((id-1) / NUM_OF_SEAT_ROWS_PER_HALL + 1))
                     .price(TICKET_PRICE)
