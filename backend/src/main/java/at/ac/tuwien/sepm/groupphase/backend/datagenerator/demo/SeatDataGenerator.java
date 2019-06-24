c768373bd355652f249cfb5299e5c7414e1c93f5
pino <e1619127@student.tuwien.ac.at>
2019-06-24 19:29:21 +0200

    Changed DataGenerators

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/demo/SeatDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/demo/SeatDataGenerator.java
@@ -1,15 +1,20 @@
 package at.ac.tuwien.sepm.groupphase.backend.datagenerator.demo;
 
 import at.ac.tuwien.sepm.groupphase.backend.datatype.PriceCategory;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Hall;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Sector;
 import at.ac.tuwien.sepm.groupphase.backend.repository.HallRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.SeatRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.SectorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Profile("generateData")
 @Component
@@ -17,13 +22,16 @@ public class SeatDataGenerator implements DataGenerator {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
     private final SeatRepository seatRepository;
+    private final SectorRepository sectorRepository;
     private final HallRepository hallRepository;
 
-    public SeatDataGenerator(SeatRepository seatRepository, HallRepository hallRepository){
+    public SeatDataGenerator(SeatRepository seatRepository, HallRepository hallRepository, SectorRepository sectorRepository) {
         this.seatRepository = seatRepository;
         this.hallRepository = hallRepository;
+        this.sectorRepository = sectorRepository;
     }
 
+    /*
     @Override
     public void generate(){
         if(seatRepository.count() > 0){
@@ -42,6 +50,45 @@ public class SeatDataGenerator implements DataGenerator {
             Seat seat10 = Seat.builder().id(10L).seatNumber(26).seatRow(10).priceCategory(PriceCategory.AVERAGE).hall(hallRepository.getOne(9L)).build();
 
             seatRepository.saveAll(Arrays.asList(seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, seat9, seat10));
+        }
+    }
+    */
+    @Override
+    public void generate() {
+        if (seatRepository.count() > 0) {
+            LOGGER.info("Seats already generated");
+        } else {
+            LOGGER.info("Generating seats");
+            List<Seat> seats = new ArrayList<>();
+            for (Long id = 1L; id <= hallRepository.count()*5; id++) {
+                Boolean seatsOrSectors = id % 2 == 0;
+                seats = seatsOrSectors ? new ArrayList<>() : null;
+                sectors = seatsOrSectors ? null : new ArrayList<>();
+                if (seatsOrSectors) {
+                    for (Long seatId = 1L; seatId <= NUM_OF_SEATS_PER_HALL; seatId++)
+                        seats.add(Seat.builder()
+                            .priceCategory(seatId % 3 == 0 ? PriceCategory.CHEAP : seatId % 3 == 1 ? PriceCategory.AVERAGE : PriceCategory.EXPENSIVE)
+                            .seatNumber(customModInt(seatId, NUM_OF_SEAT_ROWS_PER_HALL))
+                            .seatRow(Math.toIntExact((seatId - 1) / NUM_OF_SEAT_ROWS_PER_HALL + 1))
+                            .hall(hallRepository.getOne(customMod(id, NUM_OF_HALLS)))
+                            .build());
+                } else {
+                    for (Long sectorId = 1L; sectorId <= NUM_OF_SECTORS_PER_HALL; sectorId++)
+                        sectors.add(Sector.builder()
+                            .priceCategory(sectorId % 3 == 0 ? PriceCategory.CHEAP : sectorId % 3 == 1 ? PriceCategory.AVERAGE : PriceCategory.EXPENSIVE)
+                            .sectorNumber(Math.toIntExact(customMod(sectorId, NUM_OF_SECTORS_PER_HALL)))
+                            .hall(hallRepository.getOne(customMod(id, NUM_OF_HALLS)))
+                            .build());
+                }
+                halls.add(Hall.builder()
+                    .id(id)
+                    .location(locationRepository.getOne(customMod(id, NUM_OF_LOCATIONS)))
+                    .name(faker.bothify("HALL ????????"))
+                    .seats(seatsOrSectors ? seats : null)
+                    .sectors(seatsOrSectors ? null : sectors)
+                    .build());
+            }
+            hallRepository.saveAll(halls);
         }
     }
 }
